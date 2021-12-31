73f39fe5cdf64d2fef8a24a105235a27b39dd9f1
Leon Xu <dcwinfield362@gmail.com>
2021-12-31 11:09:46 -0800

    doc: add login and register route along with its component

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login";
+import Register from "../views/Register";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/logout",
+    name: "Register",
+    component: Register,
   },
 ];
 
