527888d8eadeb972b47b782bf6235f5749b40bb5
Leon Xu <dcwinfield362@gmail.com>
2022-01-01 12:43:58 -0800

    doc: update meta with title tags for each corresponding values

--- src/router/index.js
+++ src/router/index.js
@@ -10,26 +10,41 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home",
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    meta: {
+      title: "Login",
+    },
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
+    meta: {
+      title: "Register",
+    },
   },
   {
     path: "/create",
     name: "Create",
     component: Create,
+    meta: {
+      title: "Create",
+    },
   },
   {
     path: "/view-workout/:workoutId",
     name: "View Workout",
     component: ViewWorkout,
+    meta: {
+      title: "View Workout",
+    },
   },
 ];
 
